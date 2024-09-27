34ad76d77b0dda164efafe649a83bd060112c412
Pedro Kaj Kjellerup Nacht <pnacht@google.com>
2024-09-27 19:16:36 +0000

    Add wheel building to publish-to-ar.sh
    Change-Id: I342005245bbeb50ca9228406b94127218fee941a
    Signed-off-by: Pedro Kaj Kjellerup Nacht <pnacht@google.com>

--- publish-to-ar.sh
+++ publish-to-ar.sh
@@ -1,12 +1,17 @@
 set -eu
 
-pip install -U keyring > /dev/null
+pip install -U \
+    keyring \
+    twine \
+    setuptools \
+    wheel > /dev/null
 pip install -U keyrings.google-artifactregistry-auth > /dev/null
-pip install -U twine > /dev/null
 
 if ! gcloud auth application-default print-access-token --quiet > /dev/null; then
     gcloud auth application-default login
 fi
+
+python3 -m build --wheel
 
 twine upload \
     --repository-url https://us-central1-python.pkg.dev/oss-exit-gate-dev/cronk--testpypi \
