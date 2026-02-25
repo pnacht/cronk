b468fdf751948cdf70141e0f99f9e1c128e62177
Pedro Kaj Kjellerup Nacht <pnacht@google.com>
2026-02-25 19:48:29 +0000

    Test Kokoro SBOM generation
    Change-Id: I6afc5f57819353d4e63ffd5d1335e1f7e8513444
    Signed-off-by: Pedro Kaj Kjellerup Nacht <pnacht@google.com>

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -4,18 +4,22 @@ set -euo pipefail
 
 cd $KOKORO_ARTIFACTS_DIR/git/cronk
 
+echo "Installing dependencies..."
 pip install -U \
     build \
     keyring \
     twine \
     setuptools \
-    wheel > /dev/null
+    wheel \
+    requests-toolbelt > /dev/null
 
 pip install -U \
     keyrings.google-artifactregistry-auth > /dev/null
 
+echo "Building..."
 python3 -m build
 
+echo "Uploading..."
 twine upload \
     --repository-url "https://us-python.pkg.dev/oss-exit-gate-$ENV/cronk--$REGISTRY" \
     --verbose \
